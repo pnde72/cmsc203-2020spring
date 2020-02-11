013f347f7271f1b7e7445f9c39a384169c30fe3c
pnde72 <55747579+pnde72@users.noreply.github.com>
2020-02-11 11:40:48 -0500

    Add files via upload

---
+++ NumericTypes.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+/**
+   This program demonstrates how numeric types and operators behave in Java
+*/
+public class NumericTypes {
+	public static void main (String [] args) {
+		//TASK #2 Create a Scanner object here 
+		//identifier declarations
+		final int NUMBER = 2; // number of scores
+		
+		Scanner score= new Scanner(System.in);
+		
+		System.out.println("Enter first test Score: ");
+		int score1 = score.nextInt();
+		
+		System.out.println("Enter second test Score: ");
+		int score2 = score.nextInt(); 
+		
+		final int BOILING_IN_F = 212; // boiling temperature
+		double fToC; // temperature in Celsius
+		double average; // arithmetic average
+		String output; // line of output to print out
+		//Task #2 declare a variable to hold the user’s temperature
+		//Task #2 prompt user to input score1
+		//Task #2 read score1 
+		//Task #2 prompt user to input score2
+		//Task #2 read score2 
+		// Find an arithmetic average
+		average = (score1 + score2) / NUMBER;
+		output = score1 + " and " + score2 + " have an average of " + average;	
+		System.out.println(output);
+		// Convert Fahrenheit temperatures to Celsius
+		fToC = 5/9 * (BOILING_IN_F - 32);
+		output = BOILING_IN_F + " in Fahrenheit is " + fToC + " in Celsius.";
+		System.out.println(output);
+		//Task #2 prompt user to input another temperature
+		//Task #2 read the user’s temperature in Fahrenheit
+		//Task #2 convert the user’s temperature to Celsius
+		//Task #2 print the user’s temperature in Celsius
+		System.out.println("Goodbye"); // to show that the program is ended	
+	}
+}
+
